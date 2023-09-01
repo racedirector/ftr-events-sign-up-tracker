22bbdc68d5e30897dbe5e4457604d99ed1bc7269
Justin Makaila <justin@justinsowstambp.mynetworksettings.com>
2023-09-01 16:28:25 -0400

    Actually commit fixes

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -1,19 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
-import logo from "../../assets/logo.svg";
-import "./index.css";
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-MainLogo" alt="logo" />
-      </header>
-      <p>Coming soon</p>
-    </div>
-  );
-}
-=======
 import SignUps from "@/containers/SignUps";
 import "./index.css";
 
@@ -24,6 +9,5 @@ export const App = () => {
     </div>
   );
 };
->>>>>>> 9f08bb0 (Initial commit)
 
 export default App;
