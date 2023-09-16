a0a2ae69fb06025fc370f97e251910b8d90ae256
justinmakaila <1638996+justinmakaila@users.noreply.github.com>
2023-09-16 11:39:17 -0400

    use the master sheet to source the data

--- src/containers/SignUps/index.tsx
+++ src/containers/SignUps/index.tsx
@@ -1,19 +1,17 @@
 import React, { useMemo } from "react";
 import { View } from "react-native";
-import { Text, makeStyles } from "@rneui/themed";
-import {
-  ClassParticipationCountHeader,
-  ClassParticipationCountHeaderItemProps,
-} from "@/components/ClassParticipationCountHeader";
-import colors from "@/constants/theme/colors";
-import useGetSignUpsByClass from "@/hooks/useGetSignUps";
+import { Text, makeStyles, useTheme, Colors } from "@rneui/themed";
+import { ClassParticipationCountHeader } from "@/components/ClassParticipationCountHeader";
+import useGetSignups from "@/hooks/useGetSignUpsMaster";
 
-const colorForClass = (className: string) => {
+const colorForClass = (className: string, colors: Colors) => {
   switch (className) {
     case "LMP3":
       return colors.lmp3Pro;
     case "GT3":
       return colors.gt3Pro;
+    case "GT3 AM":
+      return colors.gt3Am;
     case "GT4":
       return colors.gt4Pro;
     default:
@@ -21,24 +19,48 @@ const colorForClass = (className: string) => {
   }
 };
 
+const textColorForClass = (className: string, colors: Colors) => {
+  switch (className) {
+    case "LMP3":
+      return colors.white;
+    default:
+      return colors.text;
+  }
+};
+
+const useSignUpListData = () => {
+  const {
+    theme: { colors },
+  } = useTheme();
+  const { data: { signUpCounts: signUps = {} } = {} } = useGetSignups();
+
+  const headerData = useMemo(() => {
+    return Object.keys(signUps).map((name) => {
+      const { count } = signUps[name];
+      return {
+        name,
+        count,
+        color: colorForClass(name, colors),
+        textColor: textColorForClass(name, colors),
+      };
+    });
+  }, [colors, signUps]);
+
+  return {
+    headerData,
+  };
+};
+
 export interface SignUpListProps {}
 
 export const SignUpList: React.FC<SignUpListProps> = () => {
   const styles = useStyles(makeStyles);
-  const { data = {} } = useGetSignUpsByClass();
-
-  const headerCounts: ClassParticipationCountHeaderItemProps[] = useMemo(() => {
-    return Object.entries(data).map(([name, count]) => ({
-      name,
-      count,
-      color: colorForClass(name),
-    }));
-  }, [data]);
+  const { headerData } = useSignUpListData();
 
   return (
     <View style={styles.classParticipationContainer}>
       <Text style={styles.classParticipationTitle}>Sign ups by class</Text>
-      <ClassParticipationCountHeader counts={headerCounts} />
+      <ClassParticipationCountHeader counts={headerData} />
     </View>
   );
 };
