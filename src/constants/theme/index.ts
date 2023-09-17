967f3ff0c4d6ef9c6fe80b95439726ceaf0d542a
justinmakaila <1638996+justinmakaila@users.noreply.github.com>
2023-09-17 12:35:40 -0400

    update theme

--- src/constants/theme/index.ts
+++ src/constants/theme/index.ts
@@ -1,22 +1,22 @@
-import {
-  DefaultTheme as RNDefaultTheme,
-  DarkTheme as RNDarkTheme,
-} from "@react-navigation/native";
+// import {
+//   DefaultTheme as RNDefaultTheme,
+//   DarkTheme as RNDarkTheme,
+// } from "@react-navigation/native";
 import { createTheme } from "@rneui/themed";
 import colors from "./colors";
 
 const theme = createTheme({
   darkColors: {
     ...colors,
-    text: RNDarkTheme.colors.text,
-    border: RNDarkTheme.colors.border,
-    notification: RNDarkTheme.colors.notification,
+    // text: RNDarkTheme.colors.text,
+    // border: RNDarkTheme.colors.border,
+    // notification: RNDarkTheme.colors.notification,
   },
   lightColors: {
     ...colors,
-    text: RNDefaultTheme.colors.text,
-    border: RNDefaultTheme.colors.border,
-    notification: RNDefaultTheme.colors.notification,
+    // text: RNDefaultTheme.colors.text,
+    // border: RNDefaultTheme.colors.border,
+    // notification: RNDefaultTheme.colors.notification,
   },
 });
 
